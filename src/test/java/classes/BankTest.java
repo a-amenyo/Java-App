1fbcb3aeefe693c88acfc722290ff15d87f940cf
yoooel <72613221+yoooel@users.noreply.github.com>
2021-03-18 16:21:49 +0000

    bank tests passed

--- src/test/java/classes/BankTest.java
+++ src/test/java/classes/BankTest.java
@@ -10,7 +10,19 @@ public class BankTest {
 
     @Before
     public void setUp() throws Exception {
-        bank = new Bank("TestBank");
+        Bank bank = new Bank("National Australia Bank");
+
+        bank.addBranch("Adelaide");
+
+        bank.addCustomer("Adelaide", "Tim", 50.05);
+        bank.addCustomer("Adelaide", "Jeff", 175.0);
+
+        bank.addCustomerTransaction("Adelaide","Tim", 44.22);
+
+        bank.listCustomers("Adelaide", true);
+
+
+
     }
 
     @Test
